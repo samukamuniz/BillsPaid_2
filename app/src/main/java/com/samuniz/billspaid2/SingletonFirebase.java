73014b2e73e92db565c89ee224a57217bf53c74f
samukamuniz <samuka.muniz@gmail.com>
2017-11-12 16:06:24 -0300

    Configurando o Singleton

--- app/src/main/java/com/samuniz/billspaid2/SingletonFirebase.java
+++ app/src/main/java/com/samuniz/billspaid2/SingletonFirebase.java
@@ -2,13 +2,21 @@ package com.samuniz.billspaid2;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SingletonFirebase {
 
+    private static FirebaseDatabase database;
     private static DatabaseReference referenciaFirebase;
     private static FirebaseAuth autenticacao;
+    private static FirebaseUser user;
+
+    public static FirebaseDatabase getDatabase() {
+        database = FirebaseDatabase.getInstance();
+        return database;
+    }
 
     public static DatabaseReference getReferenciaFirebase(String s) {
         if (referenciaFirebase == null){
@@ -22,5 +30,12 @@ public class SingletonFirebase {
             autenticacao = FirebaseAuth.getInstance();
         }
         return autenticacao;
+    }
+
+    public static FirebaseUser getUser() {
+        if (user == null){
+            user = autenticacao.getCurrentUser();
+        }
+        return user;
     }
 }
