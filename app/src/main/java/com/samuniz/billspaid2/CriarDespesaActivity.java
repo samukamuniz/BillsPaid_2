00514bc7901f1c7ed4d2e7332e942193ecce6d89
samukamuniz <samuka.muniz@gmail.com>
2017-11-12 14:43:18 -0300

    Criando a tela de Despesa

---
+++ app/src/main/java/com/samuniz/billspaid2/CriarDespesaActivity.java
@@ -0,0 +1,13 @@
+package com.samuniz.billspaid2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriarDespesaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_despesa);
+    }
+}
