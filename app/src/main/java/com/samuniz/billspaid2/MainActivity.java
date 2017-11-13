006ba87a5792cae690346dfa14d44418ab3b0f35
samukamuniz <samuka.muniz@gmail.com>
2017-11-13 01:46:13 -0300

    Adcionando Botão para cadastro de conta

--- app/src/main/java/com/samuniz/billspaid2/MainActivity.java
+++ app/src/main/java/com/samuniz/billspaid2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.samuniz.billspaid2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+    private Button btnConta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnConta = findViewById(R.id.btnConta);
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -36,6 +40,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();*/
             }
         });
+
+        btnConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, CriarContaActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+
+
     }
 
     @Override
